5785273b481aa5aa4e87e21eda3801a470ea1231
nerisansone <82588652+nerisansone@users.noreply.github.com>
2022-03-16 10:16:41 +0100

    Update app.py

--- HerokuProject/app.py
+++ HerokuProject/app.py
@@ -61,7 +61,7 @@ def apply_watermark():
     r1 = s3_client.upload_file(path, bucket_name, filename, ExtraArgs={'ACL': 'public-read'})
 
     # GENERATE REQUEST FOR QRACKAJACK
-    qr_req_url = "https://qrackajack.expeditedaddons.com/?api_key=" + os.environ["QRACKAJACK_API_KEY"] + "&bg_color=%23ffffff&content=http%3A%2F%2Fexample.org&fg_color=%23000000&height=256&width=256"
+    qr_req_url = "https://qrackajack.expeditedaddons.com/?api_key=" + os.environ["QRACKAJACK_API_KEY"] + "&bg_color=%23ffffff&content="+get_s3_url(bucket_name, filename)"
 
     qr_name = f"qr_{filename}"
     qr_path = request_and_save(qr_req_url, qr_name)
@@ -70,7 +70,7 @@ def apply_watermark():
 
 
     # GENERATE REQUEST FOR WATERMARKER
-    watermark_req_url = ""
+    watermark_req_url = "https://watermarker.expeditedaddons.com/?api_key=" + os.environ['WATERMARKER_API_KEY'] + '&height=100&image_url='+get_s3_url(bucket_name, filename)+'&watermark_url='+get_s3_url(bucket_name, qr_name)"
 
     watermark_name = f"watermark_{filename}"
     request_and_save(watermark_req_url, watermark_name)
